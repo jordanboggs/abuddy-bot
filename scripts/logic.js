c84b09ce002addf8ff082f93abe83bb87cdca7d1
Jordan Boggs <boggsjp@gmail.com>
2018-01-22 15:21:49 -0700

    initial commit

---
+++ scripts/logic.js
@@ -0,0 +1,20 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * 
+ * aBuddy, the Accountabilibuddy Bot             *
+ * v0.0.1 - 2018-01-22                           *
+ * Jordan Boggs                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+/*
+ * REMINDER SCHEDULE
+ * ------------
+ * Sun      Mon       Tue       Wed       Thu       Fri       Sat   
+ * Gym                Gym                 Gym                 Gym
+ * Meditate Meditate  Meditate  Meditate  Meditate  Meditate  Meditate
+ * Meds     Meds      Meds      Meds      Meds      Meds      Meds
+ * ----------------NIGHT TIME ONLY-------------------------------------
+ * To-do    To-do     To-do     To-do     To-do     To-do     To-do
+ * --------------------------------------------------------------------
+ * Once in week
+ * Deep condition and wash hair
+ */
